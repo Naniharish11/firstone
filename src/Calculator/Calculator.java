dddbc39e2b3bc14eab004cb9496a26f036a8c4ad
Naniharish7 <metikalaharish720@gmail.com>
2023-06-03 11:39:46 +0530

    my calci

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,23 @@
+package Calculator;
+
+public class Calculator {
+ public void add() {
+	 int a=1,b=2;
+	 int sum = a+b;
+	 System.out.println(sum);
+	 
+ }
+ public void sub() {
+	 int a=1,b=2;
+	 int sub = a-b;
+	 System.out.println(sub);
+	 
+ }
+ public void div() {
+	 int a=1,b=2;
+	 int div = a/b;
+	 System.out.println(div);
+ }
+ 
+ 
+}
